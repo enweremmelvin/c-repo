1aa23c427dfbeb9fe41d5c2004584940df1fbce2
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-11-24 15:07:28 +0100

    Took out execution of header functions from new.c

--- new.c
+++ new.c
@@ -7,6 +7,16 @@
 
 int main(void) {
 
-  
+  int age = 19;
+  int* ptr = &age;
 
+  printf("%p\n", ptr);
+  printf("%d\n", *ptr);
+
+  char name[] = "Melvin";
+  char* ptr_str = name;
+
+  printf("\n%s\n", name);
+  printf("%c\n", *ptr_str);
+  
 }
