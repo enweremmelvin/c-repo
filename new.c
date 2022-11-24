8c660bfa60352ff2a4e45d96d83886e3be74c519
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-11-24 10:18:57 +0100

    made slight adjustments to the new.c file

--- new.c
+++ new.c
@@ -2,14 +2,11 @@
 #include "headers/userReg.h"
 #include "headers/o-userReg.h"
 #include "headers/runningSum.h"
+#include "headers/fizzbuzz.h"
+#include "headers/arrays.h"
 
 int main(void) {
 
-  // shorthand if else - ternary operator
-  int x;
-  printf("Enter a number to be measured against 10: ");
-  scanf("%d", &x);
   
-  (x <= 10) ? printf("Yes, x is less than or equal to 10.\n") : printf("No, x is greater than 10,\n");
 
 }
